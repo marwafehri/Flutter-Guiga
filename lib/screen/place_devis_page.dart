ebbf2121762ba008fdc561fb6ac285a4ef0a74a6
Fehri Marwa <marwafehristic@gmail.com>
2024-07-16 11:28:40 +0200

    Update place_devis_page.dart
    Signed-off-by: Fehri Marwa <marwafehristic@gmail.com>

--- lib/screen/place_devis_page.dart
+++ lib/screen/place_devis_page.dart
@@ -1,5 +1,5 @@
-import 'dart:io';
 import 'dart:convert';
+import 'dart:io';  // Add this import
 import 'package:collection/collection.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
